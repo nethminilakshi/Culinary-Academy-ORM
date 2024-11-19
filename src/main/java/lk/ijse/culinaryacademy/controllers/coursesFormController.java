fc82e9c0cc0a5a4cf5b833f13e2e462fa54c2425
Nethu <nilakshinethmi48@gmail.com>
2024-11-19 14:29:26 +0530

    UI

--- src/main/java/lk/ijse/culinaryacademy/controllers/coursesFormController.java
+++ src/main/java/lk/ijse/culinaryacademy/controllers/coursesFormController.java
@@ -1,7 +1,67 @@
 package lk.ijse.culinaryacademy.controllers;
 
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class coursesFormController {
+    @FXML
+    private JFXButton btnRegister;
+
+    @FXML
+    private TableColumn<?, ?> colDuration;
+
+    @FXML
+    private TableColumn<?, ?> colFee;
+
+    @FXML
+    private TableColumn<?, ?> colProgramId;
+
+    @FXML
+    private TableColumn<?, ?> colProgramName;
+
+    @FXML
+    private TableColumn<?, ?> colRegFee;
+
+    @FXML
+    private AnchorPane programPane;
+
+    @FXML
+    private TableView<?> tblPrograms;
+
+    @FXML
+    private JFXTextField txtDuration;
+
+    @FXML
+    private JFXTextField txtFee;
+
+    @FXML
+    private JFXTextField txtId;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private TextField txtProgramName;
+
+    @FXML
+    private JFXTextField txtRegFee;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+
+    }
+
 
 }
